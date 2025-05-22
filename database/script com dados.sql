b2e28ad8cf5917f9f263166c5676a64f46da0f72
Matheus Cordeiro <cordeirogmatheus@gmail.com>
2025-05-22 18:49:22 -0300

    adicionando pasta database

---
+++ database/script com dados.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 22-Maio-2025 às 20:22
+-- Versão do servidor: 8.0.30
+-- versão do PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bunco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `licoes`
+--
+
+CREATE TABLE `licoes` (
+  `id` int NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `conteudo` text NOT NULL,
+  `tipo` enum('teoria','exercicio') NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `progresso`
+--
+
+CREATE TABLE `progresso` (
+  `id` int NOT NULL,
+  `usuario` int NOT NULL,
+  `licao` int NOT NULL,
+  `data` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `status`
+--
+
+CREATE TABLE `status` (
+  `id` int NOT NULL,
+  `usuario` int NOT NULL,
+  `vidas` int NOT NULL DEFAULT '5',
+  `ofensiva` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `status`
+--
+
+INSERT INTO `status` (`id`, `usuario`, `vidas`, `ofensiva`) VALUES
+(1, 2, 5, 0),
+(2, 3, 5, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(64) NOT NULL,
+  `link_github` varchar(255) DEFAULT NULL,
+  `link_instagram` varchar(255) DEFAULT NULL,
+  `link_linkedin` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `nome`, `email`, `senha`, `link_github`, `link_instagram`, `link_linkedin`, `created_at`) VALUES
+(1, 'joana', 'Joana', 'joana@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, NULL, '2025-04-08 19:59:47'),
+(2, 'jp_p3dro', 'Joao Pedro', 'joao@email.com', '7580adf5151c6b79c90597aeab91838f', NULL, NULL, NULL, '2025-05-20 18:38:43'),
+(3, 'antonio_rogerio', 'Antonio Rogerio', 'antonio@gmail.com', '7580adf5151c6b79c90597aeab91838f', NULL, NULL, NULL, '2025-05-21 22:03:44');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `licoes`
+--
+ALTER TABLE `licoes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `progresso`
+--
+ALTER TABLE `progresso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `licao` (`licao`);
+
+--
+-- Índices para tabela `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario` (`usuario`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `licoes`
+--
+ALTER TABLE `licoes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `progresso`
+--
+ALTER TABLE `progresso`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `progresso`
+--
+ALTER TABLE `progresso`
+  ADD CONSTRAINT `progresso_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `progresso_ibfk_2` FOREIGN KEY (`licao`) REFERENCES `licoes` (`id`);
+
+--
+-- Limitadores para a tabela `status`
+--
+ALTER TABLE `status`
+  ADD CONSTRAINT `status_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
