b0426e9e76b317303c5694dad5c7b1ca4a75603d
JpP3dro <joaopedrodallessiodebarros@gmail.com>
2025-05-25 15:25:20 -0300

    Colocando a API de alterar nome

---
+++ api/alterarNome.php
@@ -0,0 +1,42 @@
+<?php 
+header("Content-Type: application/json");
+include_once("conexao.php");
+$conn = conexao();
+
+try {
+    //Verifica se os campos foram passados
+    if (isset($_POST["username"])) {
+        $username = $_POST["username"];
+    } else {
+        echo json_encode(["sucesso" => "false", "mensagem" => "Usuário não está logado!"]);
+        exit;
+    }
+    if (isset($_POST["nomenovo"])) {
+        $nomenovo = $_POST["nomenovo"];
+    } else {
+        echo json_encode(["sucesso" => "false", "mensagem" => "O servidor não recebeu o nome novo!"]);
+        exit;
+    }
+
+    //Atualiza o nome do usuário
+    $query = "UPDATE usuarios SET nome = ? WHERE username = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(1, $nomenovo);
+    $stmt->bindParam(2, $username);
+    $resultado = $stmt->execute();
+    
+    //Verifica se a atualização do nome deu certo
+    if ($resultado) {
+        echo json_encode(["sucesso" => "true", "mensagem" => "Nome alterado com sucesso! Você será deslogado para os dados serem atualizados!"]);
+    }
+    else {
+        echo json_encode(["sucesso" => "false", "mensagem" => "Erro ao alterar o nome do usuário!"]);
+    }
+
+} catch (Exception $e) {
+    echo json_encode([
+        "sucesso" => "false",
+        "mensagem" => "Exceção: " . $e->getMessage()
+    ]);
+}
+?>
