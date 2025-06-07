4aedbd898503f120879c61baf915f46e705041d9
Matheus Cordeiro <cordeirogmatheus@gmail.com>
2025-06-07 14:26:44 -0300

    Reformulando o sobre em todas as telas

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -3,21 +3,21 @@ let integrantes = [
         id: 1,
         nome: "Gabriel Linhares",
         trabalho: "Designer",
-        imagem: 'assets/img/logo.png',
+        imagem: 'assets/img/integrantes/gabriel.svg',
         texto: 'Trabalhou na parte de design do TCC'
     },
     {
         id: 2,
         nome: "João Pedro",
         trabalho: "Programador Back-end",
-        imagem: 'assets/img/logo.png',
+        imagem: 'assets/img/integrantes/joão.svg',
         texto: 'Trabalhou na parte back-end do TCC'
     },
     {
         id: 3,
         nome: "Matheus Cordeiro",
         trabalho: "Programador Front-end",
-        imagem: 'assets/img/logo.png',
+        imagem: 'assets/img/integrantes/matheus.svg',
         texto: 'Trabalhou na parte front-end do TCC'
     }
 ]
@@ -28,6 +28,7 @@ const img = document.querySelector('#pessoa-img')
 const nome = document.querySelector('#nome')
 const trabalho = document.querySelector('#funcao')
 const texto = document.querySelector('#info')
+const info = document.querySelector('#about-info')
 
 
 document.addEventListener('DOMContentLoaded', function(e){
@@ -40,6 +41,21 @@ function mostrarPessoa(pessoa){
     nome.textContent = item.nome
     trabalho.textContent = item.trabalho
     texto.textContent = item.texto
+    if(pessoa === 0){
+        info.style.setProperty('--cor-before', '#D11515')
+        info.style.setProperty('--cor-texto', '#D11515')
+        document.body.style.setProperty('--cor-texto', '#D11515')
+    }
+    if(pessoa === 1){
+        info.style.setProperty('--cor-before', '#3E7500')
+        info.style.setProperty('--cor-texto', '#3E7500')
+        document.body.style.setProperty('--cor-texto', '#3E7500')
+    }
+    if(pessoa === 2){
+        info.style.setProperty('--cor-before', '#066693')
+        info.style.setProperty('--cor-texto', '#066693')
+        document.body.style.setProperty('--cor-texto', '#066693')
+    }
 }
 
 document.querySelector('.next-btn').addEventListener('click', function(){
