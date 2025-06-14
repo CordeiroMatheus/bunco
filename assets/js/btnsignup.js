06feaefbc9ccbe3f063d33a856c9547c5bd108b9
Matheus Cordeiro <cordeirogmatheus@gmail.com>
2025-06-14 17:16:49 -0300

    Adicionando animações e outros estilos

--- assets/js/btnsignup.js
+++ assets/js/btnsignup.js
@@ -1,8 +1,8 @@
 let btn = document.querySelector('#signup-button')
 btn.addEventListener('click', function(){
 btn.disabled = true
-btn.style.cursor = "none"
+btn.style.cursor = "default"
 setTimeout(function(){
 document.location.href = "pages/signup.html"
-}, 1000)
+}, 600)
 })
