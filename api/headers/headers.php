7384c39dd365d1dcce38a3166d6b4605e0958f89
JpP3dro <joaopedrodallessiodebarros@gmail.com>
2025-09-12 22:30:08 -0300

    Arrumando o CORS do header

--- api/headers/headers.php
+++ api/headers/headers.php
@@ -1,18 +1,7 @@
 <?php
-// Domínios permitidos
-$dominiosPermitidos = [
-    "http://localhost",
-    "http://127.0.0.1",
-    "https://flutlab.io",
-    "https://ftp-bunco.alwaysdata.net"
-];
-
-// Verifica se a origem do request é válida
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $dominiosPermitidos)) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-}
 
 // Headers e métodos permitidos
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
